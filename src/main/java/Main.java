7987094a9a9269deb4196df356bccb0b5fcdfd8b
ibrohim <nabiyevibrohim77@gmail.con>
2023-05-31 09:59:47 +0500

    just

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,7 @@
+import order.OrderRepository;
+
 public class Main {
+    public static void main(String[] args) {
+        OrderRepository orderRepository = new OrderRepository();
+    }
 }
--- src/main/java/order/OrderRepository.java
+++ src/main/java/order/OrderRepository.java
@@ -2,10 +2,22 @@ package order;
 
 import common.BaseRepository;
 
+import java.io.File;
+import java.io.IOException;
+
 public class OrderRepository extends BaseRepository<Order> {
-    private final String FILE_PATH="src/main/java/order/order.txt";
+    private final String FILE_PATH = "src/main/java/order/order.txt";
+
     @Override
     protected String getPath() {
-        return null;
+        File file = new File(FILE_PATH);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return FILE_PATH;
     }
 }
