57cd734b0e23ed0a32aace379eb8f0d8f7b9ccb5
ILYOSOV JAVOHIR <javohirilyasov26@gmail.com>
2023-05-31 12:28:27 +0500

    New

---
+++ src/main/java/task1/AnswerForTaskOne.java
@@ -0,0 +1,23 @@
+package task1;
+
+public class AnswerForTaskOne {
+//    Javob:
+//
+//    1.Race condition - ikkita yoki undan ko'p threadlar birxil kelib qolsa race condition bo'ladi.
+//
+//    Yechim
+//
+//    Race conditiondan qutilish uchun 3ta yo'l bor:
+//
+//     1. Atomic obyekti bilan qutilsak bo'ladi
+//     2. Lock interfasi bilan qutilsak bo'ladi
+//    uning ichida lock va unlock methodi bor.
+//     3. synchronized modefier bor
+//     uni ikki xil yol bilan yozsek bo'ladi
+//     3.1 methodda midifier accsessidan keyn
+//     3.2 blokga olib
+//
+//    Uchalasida tez ishlidigani bu Atomic obyekti chunki qogan ikkitasi obyektni kuttiradi, a Atomic obyecti obyektni lock qilganda boshqa
+//    threadlar kutib qolmidi.
+
+}
